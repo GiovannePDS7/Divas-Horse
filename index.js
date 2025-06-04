1f47b7b0d2933331d23f1873ca73f2d763710e4d
GiovannePDS7 <giovanne3282@gmail.com>
2025-06-04 12:25:48 -0300

    Subindo arquivos base

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function InicialComecar() {
+    var containerInicial = document.getElementById('containerInicial')
+    var containerDadosCavalo = document.getElementById('containerDadosCavalo')
+
+    containerInicial.classList.add('d-none')
+    containerInicial.classList.remove('d-flex')
+    containerDadosCavalo.classList.add('d-flex')
+    containerDadosCavalo.classList.remove('d-none')
+}
+var qtdCavalos = 0;
+function QtdCavalosContinuar(){
+    
+}
